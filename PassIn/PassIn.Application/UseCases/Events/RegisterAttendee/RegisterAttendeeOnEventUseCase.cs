ebb34bddf54a3a1f8ba4f8f6887928a9acdbffb9
Bianca Santo <bianca.ces81@gmail.com>
2024-04-04 11:12:24 -0400

    NLW- C#- PassIn

---
+++ PassIn/PassIn.Application/UseCases/Events/RegisterAttendee/RegisterAttendeeOnEventUseCase.cs
@@ -0,0 +1,84 @@
+﻿using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using System.Net.Mail;
+
+namespace PassIn.Application.UseCases.Events.RegisterAttendee;
+
+public class RegisterAttendeeOnEventUseCase
+{
+    private readonly PassInDbContext _dbContext;
+
+    public RegisterAttendeeOnEventUseCase()
+        {
+        _dbContext = new PassInDbContext();
+        }
+
+    public ResponseRegisterJson Execute(Guid eventId, RequestRegisterEventJson request)
+    {
+        Validate(eventId, request);
+
+        var entity = new Infrastructure.Entities.Attendee
+        {
+            Email = request.Email,
+            Name = request.Name,
+            Created_At = DateTime.UtcNow,
+        };
+
+        _dbContext.Attendees.Add(entity);
+        _dbContext.SaveChanges();
+
+        return new ResponseRegisterJson
+        {
+            Id = entity.Id,
+    };
+    }
+
+    private void Validate(Guid eventId, RequestRegisterEventJson request)
+    {
+        var eventEntity = _dbContext.Events.Find(eventId);
+        if (eventEntity is null)
+            throw new NotFoundException("An event this id does not exist.");
+
+        if (string.IsNullOrWhiteSpace(request.Name))
+        {
+            throw new ErrorOnValidationException("The name is invalid.");
+        }
+
+        var emailIsValid = EmailIsValid(request.Email);
+        if(emailIsValid == false)
+        {
+            throw new ErrorOnValidationException("The e-mails is invalid.");
+        }
+
+        var attendeeAlreadyRegistered = _dbContext
+            .Attendees
+            .Any(attendee => attendee.Email.Equals(request.Email) && attendee.Event_Id == eventId);
+
+        if (attendeeAlreadyRegistered)
+        {
+            throw new ErrorOnValidationException("You can not register twice on the same event.");
+        }
+
+        var attendeesForEvent = _dbContext.Attendees.Count(attendee => attendee.Event_Id == eventId);
+        if(attendeesForEvent == eventEntity.Maximum_Attendees)
+        {
+            throw new ErrorOnValidationException("The is no room for this event.");
+        }
+    }
+
+    private bool EmailIsValid(string email)
+    {
+        try
+        {
+            new MailAddress(email);
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
